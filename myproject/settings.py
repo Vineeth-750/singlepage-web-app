c28cc987e4ba5398859b16ef8a53025e5110a91e
vineeth <vineeth.nair997@gmail.com>
2026-08-22 15:13:31 +0530

    added allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n!unnsquwkyxe^hf-=bsluxm47%4df84zjv2oeog^ndg7d#*%l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
